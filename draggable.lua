1af171b71b393c8cfadaf18680e1787c1afe9506
vxv1337 <127777227+vxv1337@users.noreply.github.com>
2024-12-25 20:01:42 +0000

    Create draggable.lua

---
+++ draggable.lua
@@ -0,0 +1,30 @@
+local dragToggle = nil
+local dragSpeed = 0.25
+local dragInput = nil
+local dragStart = nil
+local startPos = nil
+
+local function update(input)
+    local delta = input.Position - dragStart
+    Maiin.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+end
+
+Maiin.InputBegan:Connect(function(input, gameProcessed)
+    if gameProcessed then return end
+    if input.UserInputType == Enum.UserInputType.MouseButton1 then
+        dragToggle = true
+        dragStart = input.Position
+        startPos = Maiin.Position
+        input.Changed:Connect(function()
+            if dragToggle then
+                update(input)
+            end
+        end)
+    end
+end)
+
+Maiin.InputEnded:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseButton1 then
+        dragToggle = false
+    end
+end)
